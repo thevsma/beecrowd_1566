d08f9a64afa013c799f9a852e6a1d59de9f6494d
Matheus Vieira <matheus.vieiraprates@gmail.com>
2025-08-18 22:11:13 -0300

    final commit

--- main.c
+++ main.c
@@ -1,82 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void trocar(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void mediana(int arr[], int ini, int fim) {
-    int meio = ini + (fim - ini) / 2;
-
-    if (arr[ini] > arr[meio]) {
-        trocar(&arr[ini], &arr[meio]);
-    }
-    if (arr[ini] > arr[fim]) {
-        trocar(&arr[ini], &arr[fim]);
-    }
-    if (arr[meio] > arr[fim]) {
-        trocar(&arr[meio], &arr[fim]);
-    }
-    trocar(&arr[meio], &arr[fim]);
-}
-
-int partir(int arr[], int ini, int fim) {
-    int pivo = arr[fim], i = (ini - 1);
-
-    for (int j = ini; j < fim; j++) {
-        if (arr[j] <= pivo) {
-            i++;
-            trocar(&arr[i], &arr[j]);
-        }
-    }
-
-    trocar(&arr[i + 1], &arr[fim]);
-    return (i + 1);
-}
-
-void quicksort(int arr[], int ini, int fim) {
-    while (ini < fim) {
-        mediana(arr, ini, fim);
-        int indice = partir(arr, ini, fim);
-
-        if (indice - ini < fim - indice) {
-            quicksort(arr, ini, indice - 1);
-            ini = indice + 1;
-        }
-        else {
-            quicksort(arr, indice + 1, fim);
-            fim = indice - 1;
-        }
-    }
-}
+#define MAX_ALTURA 231  // 230 + 1 (índice 0 não será usado)
+#define MIN_ALTURA 20
 
 int main() {
-    int NC;
+    int NC, N, altura;
+    int cont[MAX_ALTURA]; // O índice representa a altura
 
-    scanf("%d",&NC);
-    if (NC == 0) return 0;
+    scanf("%d", &NC);
+    while (NC--) {
+        scanf("%d", &N);
 
-    int N;
-    while (NC > 0) {
-        scanf("%d",&N);
-        int* arr = (int*)malloc(N*sizeof(int));
+        // Zera o contador de alturas
+        for (int i = 0; i < MAX_ALTURA; i++) cont[i] = 0;
 
+        // Conta cada altura lida
         for (int i = 0; i < N; i++) {
-            scanf("%d", &arr[i]);
+            scanf("%d", &altura);
+            cont[altura]++;
         }
 
-        quicksort(arr, 0, N - 1);
-
-        for (int i = 0; i < N; i++) {
-            printf("%d%c", arr[i], i == N - 1 ? '\0' : ' ');
+        // Imprime as alturas ordenadas
+        int primeiro = 1;
+        for (int i = MIN_ALTURA; i < MAX_ALTURA; i++) {
+            while (cont[i]--) {
+                if (!primeiro) printf(" ");
+                printf("%d", i);
+                primeiro = 0;
+            }
         }
         printf("\n");
-
-        free(arr);
-        NC--;
     }
-
     return 0;
 }
